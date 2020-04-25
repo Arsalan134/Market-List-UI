c545525057c291392f12e681a71c8f9580933ecb
Arsalan <airavani2018@ada.edu.az>
2020-04-25 18:45:57 +0200

    gitignore

---
+++ Market List UITests/Market_List_UITests.swift
@@ -0,0 +1,34 @@
+//
+//  Market_List_UITests.swift
+//  Market List UITests
+//
+//  Created by Arsalan Iravani on 24/04/2020.
+//  Copyright © 2020 Arsalan Iravani. All rights reserved.
+//
+
+import XCTest
+@testable import Market_List_UI
+
+class Market_List_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
